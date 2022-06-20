51583305d05d08de44bc7e128ecfe15fe793bfc9
Dmitrii Trebin <d.trebin@rambler.ru>
2022-06-20 21:04:29 +0300

    E

--- src/test/java/ru/netology/delivery/data/UserInfo.java
+++ src/test/java/ru/netology/delivery/data/UserInfo.java
@@ -1,11 +1,11 @@
 package ru.netology.delivery.data;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
+@RequiredArgsConstructor
 
 public class UserInfo {
 
